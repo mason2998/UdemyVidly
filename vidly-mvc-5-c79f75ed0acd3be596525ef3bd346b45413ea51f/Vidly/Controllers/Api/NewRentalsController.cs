7e7065ce82d582a2bdb89cfa92c72dffb7949b0a
mason2998 <mason2998@hotmail.com>
2021-04-06 13:19:33 +0100

    Implemented Api and added rental class to DB

--- vidly-mvc-5-c79f75ed0acd3be596525ef3bd346b45413ea51f/Vidly/Controllers/Api/NewRentalsController.cs
+++ vidly-mvc-5-c79f75ed0acd3be596525ef3bd346b45413ea51f/Vidly/Controllers/Api/NewRentalsController.cs
@@ -5,15 +5,37 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.Dtos;
+using System.Data.Entity;
+using Vidly.Models;
 
 namespace Vidly.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
+        private ApplicationDbContext _context;
         [HttpPost]
         public IHttpActionResult CreateRental(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers.
+                Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.
+                Where(m => newRental.MovieIds.
+                Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rentals
+                {
+                    customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+            _context.SaveChanges();
+            return Ok();
         }
     }
 }
